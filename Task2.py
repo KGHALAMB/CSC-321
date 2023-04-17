c50e31537392ed4d4018368cabcef2332cdb3c00
DakshG <dakshgopalani@gmail.com>
2023-04-17 13:12:51 -0700

    alternate to byte flipping

--- Task2.py
+++ Task2.py
@@ -18,15 +18,23 @@ def submit(input_string, key, iv):
 def verify(input_string, key, iv):
     cipher = AES.new(key, AES.MODE_CBC, iv)
     plaintext = (Task1.pkcs7_unpadding(cipher.decrypt(input_string))).decode('utf-8')
-    print(plaintext)
+    plaintext = replaceURL(plaintext)
+    #print(plaintext)
     res = plaintext.find(";admin=true;")
     if res == -1:
         return False
     else:
         return True
+    
+def replaceURL(cipher_text):
+    cipher_text = cipher_text.replace("%3D", "=")
+    cipher_text = cipher_text.replace("%3B", ";")
+    return cipher_text
 
 def byteFlip(cipher_text):
     print(cipher_text)
 
-byteFlip(submit(";admin=true;", key, iv))
-print(verify(submit(";admin=true;", key, iv),key,iv))
+
+#byteFlip(submit("userid=456;userdata=You’re the man now, dog;session-id=31337", key, iv))
+print(verify(submit("userid=456;userdata=You’re the man now, dog;session-id=31337", key, iv),key,iv))
+print(verify(submit("userid=456;userdata=You’re the man now, dog;session-id=31337;admin=true", key, iv),key,iv))
