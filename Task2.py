5c3d6edfcfee3b2738b56e3cbcbf0b250048abf7
kavehghalambor <kghalamb@calpoly.edu>
2023-04-16 21:44:10 -0700

    still trying to do byte flipping

--- Task2.py
+++ Task2.py
@@ -4,15 +4,29 @@ import os
 
 HEADER_SIZE = 54  # bytes
 BLOCK_SIZE = 16 #16 Bytes = 128 bits
+key = Task1.make_key()
+iv = Task1.make_iv()
 
-def submit(input_string):
+def submit(input_string, key, iv):
     res = input_string.replace(";", "%3B")
-    res = res.replace("+", "%2B")
+    res = res.replace("=", "%3D")
     res = "userid=456;userdata=" + res + ";session-id=31337"
     res_byte = res.encode('utf-8')
     padded_res = Task1.pkcs7_padding(res_byte)
-    key = Task1.make_key()
-    iv = Task1.make_iv()
     return Task1.cbc_encrypt(key,iv,padded_res)
 
-print(submit("You're;the+man now dog!"))
+def verify(input_string, key, iv):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = (Task1.pkcs7_unpadding(cipher.decrypt(input_string))).decode('utf-8')
+    print(plaintext)
+    res = plaintext.find(";admin=true;")
+    if res == -1:
+        return False
+    else:
+        return True
+
+def byteFlip(cipher_text):
+    print(cipher_text)
+
+byteFlip(submit(";admin=true;", key, iv))
+print(verify(submit(";admin=true;", key, iv),key,iv))
