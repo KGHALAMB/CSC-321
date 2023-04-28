f5d13d843e0351f53b564b38a2c272fcbf43b8aa
DakshG <dakshgopalani@gmail.com>
2023-04-28 16:59:20 -0700

    works now

--- asgn4/Task2.py
+++ asgn4/Task2.py
@@ -8,6 +8,7 @@ from nltk.corpus import words
 start_time = time.time()
 word_list = words.words()
 filtered_words = [word for word in word_list if len(word) >= 6 and len(word) <= 10]
+filtered_words.sort()
 
 
 # Open the PDF file in binary mode
@@ -21,27 +22,39 @@ num_pages = len(pdf_reader.pages)
 page = pdf_reader.pages[0]
 text = page.extract_text()
 lines = text.split('\n')
+
+i = 0
 for line in lines:
     parts = line.split('$')
+    i += 1
+    if i == 2:
+        break
 print(parts)
 
 #Test
+'''
 test_word = "registrationsucks"
 test_encode = "$2b$08$J9FW66ZdPI2nrIMcOxFYI."
 ret = bcrypt.hashpw(test_word.encode('utf-8'), test_encode.encode('utf-8'))
 print(ret)
 
 print(bcrypt.checkpw(test_word.encode('utf-8'), ret))
+'''
 
 name = parts[0]
 salthash = '$' + parts[1] + '$' + parts[2] + '$' + parts[3]
-print(name)
-print(salthash)
-for word in filtered_words:
-    if bcrypt.checkpw(word.encode('utf-8'), salthash.encode('utf-8')):
-        print(name)
-        print(word) 
-        break
+salthash = salthash.strip()
+sh = salthash.encode('utf-8')
 
+print(name + " " + salthash)
+
+for word in filtered_words:
+    ret = bcrypt.hashpw(word.encode('utf-8'), sh)
+    #print(ret)
+    #print(sh)
+    if ret == sh:
+        print(name + " " + word)
+        break
+    
 end_time = time.time() - start_time
 print(end_time)
